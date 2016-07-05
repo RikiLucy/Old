da02099aa61e711b96ff562ff46064bf1a5eced0
RikiLucy <riki.and.lucy@gmail.com>
2016-07-05 16:39:40 +0700

    Добавил маршрут на главную страницу и макет для нее

---
+++ resources/views/articles.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main')
+@section('content')
+
+
+Статьи
+
+
+@endsection('content')
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>BLOG</title>
+        <meta charset="utf-8">
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <link rel="stylesheet" href="main.css">
+        
+        
+    </head>
+    <body>
+        <div class="container">
+            
+            <div class="jumbotron">
+                <a  href="/" ><h1>Laravel Blog</h1></a>
+  
+  
+            </div>
+        
+        
+            <div class="content">
+                @yield('content')
+            </div>
+        </div>
+        <div class="panel-footer">footer<br><br></div>
+    </body>
+</html>
